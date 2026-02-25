1b6422395f2cfc0d689c1168ffe82025a53f1543
heohyeon <doubleh86@gmail.com>
2026-02-25 23:43:43 +0900

    readme 추가 이메일 중복 체크

--- TaskTest/UploadEmployeeHandlerTest.cs
+++ TaskTest/UploadEmployeeHandlerTest.cs
@@ -1,5 +1,6 @@
 using Task_20260225.Command;
 using Task_20260225.Handlers.Commands;
+using Task_20260225.Models;
 using Task_20260225.Services;
 using Task_20260225.Utils;
 
@@ -49,6 +50,34 @@ public class UploadEmployeeHandlerTest
         using var sut = new UploadEmployeeInfoHandler(command, cache);
 
         Assert.ThrowsAsync<ServerException>(async () => await sut.HandleAsync());
+    }
+
+    [Test]
+    public async Task HandleAsync_WithDuplicateEmail_SkipsDuplicate()
+    {
+        var cache = new ContactCacheService();
+        cache.AddContactList([
+            new ContactModel
+            {
+                Name = "기존사용자",
+                Email = "dup@test.com",
+                Phone = "010-0000-0000",
+                Date = "2026.02.25"
+            }
+        ]);
+
+        var json = """
+                   [{"name":"신규사용자","email":"DUP@test.com","phone":"010-1111-2222","date":"2026.02.25"}]
+                   """;
+        var command = new UploadEmployeeInfoCommand(json);
+        using var sut = new UploadEmployeeInfoHandler(command, cache);
+
+        var result = await sut.HandleAsync();
+
+        Assert.That(result, Is.EqualTo(1));
+        var contacts = cache.GetContactList(1, 10);
+        Assert.That(contacts.Count, Is.EqualTo(1));
+        Assert.That(contacts[0].Name, Is.EqualTo("기존사용자"));
     }
 
     private static string GetTestDataPath(string fileName)
